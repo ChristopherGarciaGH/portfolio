edb1bea0bbfcdec038bdbf531424dc4ade85d053
ChrisGarciaGitHub <83058410+ChrisGarciaGitHub@users.noreply.github.com>
2023-12-18 20:21:52 +0100

    Se ha agregado un nuevo ejercicio

---
+++ PooEnCSharp/HappyNumber.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasTecnicas
+{
+    public class ResultadoHappyNumber()
+    {
+        public bool Sucess { get; set; } = true;
+
+    }
+    internal class HappyNumber
+    {
+        private int _n = 0;
+
+        public int N { get { return _n; } }
+
+        public HappyNumber(int n)
+        {
+            this._n = n;
+        }
+
+        public ResultadoHappyNumber isHappyNumber()
+        {
+            ResultadoHappyNumber resultadoHappyNumber = new ResultadoHappyNumber();
+            if (_n <= 0)
+            {
+                resultadoHappyNumber.Sucess = false;
+                return resultadoHappyNumber;
+            }
+            else
+            {
+                //Convertir int a string
+                String Numero = _n.ToString();
+                List<int> NumeroSeparado = new List<int>();
+
+
+                foreach (var item in Numero)
+                {
+                    if (int.TryParse(item.ToString(), out int NumeroLista))
+                    {
+                        NumeroSeparado.Add(NumeroLista);
+                    }
+                }
+
+                /*
+                 * Una vez tenemos la lista con los numeros separados tenemos que 
+                 * hacer otra lista, se utilizará HashSet porque no permite numeros
+                 * duplicados.
+                 * Tenemos que guardar en una lista los numeros vistos ya que 
+                 * tenemos el riesgo de hacer un bucle infinito
+                 */
+                HashSet<int> NumerosVistos = new HashSet<int>();
+                /*El bucle debe continuar mientras _n sea distinto de 1 y 
+                 * el numero no se encuentre en la lista NumerosVistos
+                 */
+                while (_n != 1 && !NumerosVistos.Contains(_n))
+                {
+                    NumerosVistos.Add(_n);
+                    int SumaCuadrados = 0;
+
+                    foreach (int item in NumeroSeparado)
+                    {
+                        SumaCuadrados += item * item;
+                    }
+                    /*
+                     * Tenemos que volver a reiniciar la lista Numero separado
+                     * ya que tenemos que volver a calcular los cuadrados de
+                     * los numeros de la lista 
+                     */
+                    NumeroSeparado = new List<int>();
+                    foreach (var item in SumaCuadrados.ToString())
+                    {
+                        if(int.TryParse(item.ToString(), out int NumeroLista))
+                        {
+                            NumeroSeparado.Add(NumeroLista);
+                        }
+                    }
+
+                    _n = SumaCuadrados;
+                }
+                
+                return resultadoHappyNumber;
+            }
+        }
+    }
+}
--- PooEnCSharp/Program.cs
+++ PooEnCSharp/Program.cs
@@ -1,10 +1,13 @@
 ﻿using PooEnCSharp;
+using PruebasTecnicas;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        /*
+         * Ejercicio palindromo
+         * 
         Palindromo p = new Palindromo("anita lava la tina");
         ResultadoPalindromo rs = p.esPalindromo();
         if (rs.Sucess == true)
@@ -16,7 +19,11 @@ internal class Program
             Console.WriteLine("No es palindromo");
         }
 
+        */
 
+        /*
+         * Ejercicio TwoSum
+         * 
         int[] numbers = { 7, 25, 11, 2 };
         int target = 9;
         
@@ -30,5 +37,21 @@ internal class Program
         {
             Console.WriteLine("No se ha encontrado solucion");
         }
+
+        */
+
+        int Numbers = 19;
+        HappyNumber hn = new HappyNumber(Numbers);
+        ResultadoHappyNumber Resultado = hn.isHappyNumber();
+
+        if (Resultado.Sucess == true)
+        {
+            Console.WriteLine($"El numero {Numbers} es un numero feliz");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {Numbers} no es un numero feliz");
+        }
+
     }
 }
