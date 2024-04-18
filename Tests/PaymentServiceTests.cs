aa1dd5bc584ba606ea62439f311ba3033b215bad
ChristopherGarciaGH <83058410+ChristopherGarciaGH@users.noreply.github.com>
2024-04-18 16:20:39 +0200

    Add files via upload

---
+++ Tests/PaymentServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using Moq;
+
+public interface IPaymentGateway
+{
+    bool ProcessPayment(decimal amount);
+}
+
+public class PaymentService
+{
+    private IPaymentGateway _paymentGateway;
+    
+    public PaymentService(IPaymentGateway paymentGateway)
+    {
+        _paymentGateway = paymentGateway;
+    }
+
+    public bool ProcessPayment(decimal amount)
+    {
+        // Logica de validacion
+
+        // Cualquir otro tipo de logica de la unicad de codidgo.
+
+        // Lógica de negocio para procesar el pago
+        return _paymentGateway.ProcessPayment(amount);
+    }
+
+}
+
+[TestFixture]
+public class PaymentServiceTests
+{
+    [Test]
+    public void ProcessPayment_Should_Return_True_If_Payment_Is_Successful()
+    {
+        // Arrange
+        var amount = 100m;
+
+        var paymentGatewayMock = new Mock<IPaymentGateway>();
+        paymentGatewayMock.Setup(gateway => gateway.ProcessPayment(amount)).Returns(true);
+
+        var paymentService = new PaymentService(paymentGatewayMock.Object);
+
+        // Act
+        var result = paymentService.ProcessPayment(amount);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+}
